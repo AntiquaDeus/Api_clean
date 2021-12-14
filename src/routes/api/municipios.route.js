59225afd72e64191f820d24481a06caa4e8493ac
Antonio Pardini <antoniopardini39@outlook.es>
2021-12-13 19:59:10 -0700

    primer commit

---
+++ src/routes/api/municipios.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const municipios = require('../../controllers/municipios.controller');
+
+router.get('/', municipios.GetMunicipios);
+router.get('/id', municipios.GetMunicipioById);
+router.post('/create', municipios.InsertMunicipio);
+router.put('/update', municipios.UpdateMunicipio);
+
+module.exports = router;
